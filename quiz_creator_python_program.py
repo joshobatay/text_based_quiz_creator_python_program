8d7b722e6a2d34b31f6ff4519adb015b20c097e6
Josh Obatay <joshobatay2005@gmail.com>
2025-04-08 13:05:41 +0800

    added additional comments, started creating the main menu of the program

--- quiz_creator_python_program.py
+++ quiz_creator_python_program.py
@@ -4,9 +4,16 @@ from colorama import Fore # Module for colored text
 import pyfiglet # Module for ASCII art
 import json # Module for JSON handling
 
+# Front page of the program
+text = pyfiglet.figlet_format(text = "Welcome to the Quiz Creator!", font = "ansi_regular", width = 150)
+print(Fore.CYAN + text + Fore.RESET) # Print the welcome message in cyan color, reset color after
 
-text = pyfiglet.figlet_format(text = "Welcome to the Quiz Creator!", font = "ansi_regular", width = 100)
-print(Fore.CYAN + text)
+# Main menu of the program
+print("""
+      1. Create your own quiz
+      2. Meet the developer
+      3. Exit the program
+      """)
 
 
 '''
