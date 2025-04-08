0484014b107359cdcd2b7877f0a4f76b7358491c
Josh Obatay <joshobatay2005@gmail.com>
2025-04-08 13:38:06 +0800

    added while true, new problems arise, meet the developer wont show up properly due to while true, ill assume it will be the same if create your own quiz was entered

--- quiz_creator_python_program.py
+++ quiz_creator_python_program.py
@@ -5,27 +5,29 @@ import pyfiglet # Module for ASCII art
 import json # Module for JSON handling
 
 # Front page of the program
-def front_page():
-    text = pyfiglet.figlet_format(text = "Welcome to the Quiz Creator!", font = "ansi_regular", width = 150) # Create ASCII art for the welcome message
-    print(Fore.CYAN + text + Fore.RESET) # Print the welcome message in cyan color, reset color after
+def main_menu():
+    while True:
+        text = pyfiglet.figlet_format(text = "Welcome to the Quiz Creator!", font = "ansi_regular", width = 150) # Create ASCII art for the welcome message
+        print(Fore.CYAN + text + Fore.RESET) # Print the welcome message in cyan color, reset color after
 
-    # Main menu of the program
-    print("""
-1. Create your own quiz
-2. Meet the developer
-3. Exit the program
-        """)
-    user_choice = input("Enter your choice: ").strip().lower() # Get user input for the choice
-    if user_choice in ["1", "one", "create your own quiz"]:
-        quiz_creator()
-    elif user_choice in ["2", "two", "meet the developer"]:
-        developer_info()
-    elif user_choice in ["3", "three", "exit the program"]:
-        clear_screen()
-        print("Exiting the program...")
-    else:
-        print("Invalid choice. Please try again.")
-        
+        # Main menu of the program
+        print("""
+    1. Create your own quiz
+    2. Meet the developer
+    3. Exit the program
+            """)
+        user_choice = input("Enter your choice: ").strip().lower() # Get user input for the choice
+        if user_choice in ["1", "one", "create your own quiz"]:
+            quiz_creator()
+        elif user_choice in ["2", "two", "meet the developer"]:
+            developer_info()
+        elif user_choice in ["3", "three", "exit the program"]:
+            clear_screen()
+            print("Exiting the program...")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+            
 
 
 def quiz_creator():
@@ -53,4 +55,4 @@ def developer_info():
 Program made by: BSCpE 1-6 | Gabriel Josh A. Obatay
 """ + Fore.RESET) # Print developer information in green color, reset color after
     
-front_page()
+main_menu()
