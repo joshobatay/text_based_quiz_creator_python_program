ff88249edb9a6891e22d2c82e87647b67a5ab604
Josh Obatay <joshobatay2005@gmail.com>
2025-04-08 12:22:27 +0800

    test code is now fully working, getting ready for full coding process

--- quiz_creator_python_program.py
+++ quiz_creator_python_program.py
@@ -2,17 +2,21 @@
 
 import json
 
-cars = {
-    "brand": "Toyota",
-    "model": "Corolla",
-    "year": 1983,
+quiz_question = {
+    "Question": "When was the Philippines discovered?",
+    "Choices": {"A.": "1521", 
+                "B.": "1777", 
+                "C.": "1609", 
+                "D.": "1236"
+                },
+    "Correct Answer": "A. 1521"
 }
 
 destination = "C:/Users/josho/OneDrive/Desktop/output.json"
 
 try:
     with open(destination, "w") as file:
-        json.dump(cars, file, indent = 4)
+        json.dump(quiz_question, file, indent = 4)
         print("JSON file created successfully.")
 except FileExistsError:
     print("File already exists")
