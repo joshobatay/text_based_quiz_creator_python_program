fcd7480a8d2c2e0e442a9a0522438af5bb803e66
Josh Obatay <joshobatay2005@gmail.com>
2025-04-08 13:53:31 +0800

    made changes to spacings, removed the break function under the quiz_creator() and developer_info(), main menu now working properly

--- quiz_creator_python_program.py
+++ quiz_creator_python_program.py
@@ -13,17 +13,15 @@ def main_menu():
 
         # Main menu of the program
         print("""
-    1. Create your own quiz
-    2. Meet the developer
-    3. Exit the program
+1. Create your own quiz
+2. Meet the developer
+3. Exit the program
             """)
         user_choice = input("Enter your choice: ").strip().lower() # Get user input for the choice
         if user_choice in ["1", "one", "create your own quiz"]:
             quiz_creator()
-            break
         elif user_choice in ["2", "two", "meet the developer"]:
             developer_info()
-            break
         elif user_choice in ["3", "three", "exit the program"]:
             clear_screen()
             print("Exiting the program...")
@@ -54,7 +52,7 @@ def developer_info():
     while True:
         clear_screen()
         print(Fore.GREEN + """
-    Program made by: BSCpE 1-6 | Gabriel Josh A. Obatay
+Program made by: BSCpE 1-6 | Gabriel Josh A. Obatay
     """ + Fore.RESET) # Print developer information in green color, reset color after
         choice = input("Press 'b' to go back to the main menu: ").strip().lower() 
         if choice == "b":
