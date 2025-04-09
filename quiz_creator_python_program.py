26a4547210caac298a64eab2b4b6c90f5b7f37eb
Josh Obatay <joshobatay2005@gmail.com>
2025-04-09 10:42:40 +0800

    added more codes, now testing the program to see how it works

--- quiz_creator_python_program.py
+++ quiz_creator_python_program.py
@@ -59,21 +59,31 @@ def quiz_creator():
         explanation = input("Enter the explanation: ").strip() # Ask user for the explanation of the correct answer
         
 
-    quiz_question = {
+        quiz_question = {
+            "Question": question,
+            "Choices": {
+                "A": choice_A,
+                "B": choice_B,
+                "C": choice_C,
+                "D": choice_D
+                
+            },
+            "Correct Answer": correct_answer,
+            "Explanation": explanation,
+            
+        }
         
+        quiz_data["questions"][question] = quiz_question
 
+        desktop_path = Path.home() / "Desktop" # Path of the user's Desktop
+        destination = desktop_path / "create_your_own_quiz.json" # Destination for the JSON file
 
-    }
-
-    desktop_path = Path.home() / "Desktop" # Path of the user's Desktop
-    destination = desktop_path / "create_your_own_quiz.json" # Destination for the JSON file
-
-    try:
-        with open(destination, "w") as file:
-            json.dump(quiz_question, file, indent = 4)
-            print("JSON file created successfully.")
-    except FileExistsError:
-        print("File already exists")
+        try:
+            with open(destination, "w") as file:
+                json.dump(quiz_question, file, indent = 4)
+                print("JSON file created successfully.")
+        except FileExistsError:
+            print("File already exists")
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear') # Clear the screen for Windows or Unix-based systems
