f32abd3f6c8e9ecab3acf782913c78584a15c11b
Josh Obatay <joshobatay2005@gmail.com>
2025-05-03 14:20:58 +0800

    fixed the error where program crashes when the target file is not found, so I opted for a more flexible option by using tkinter library and asks the user where is the json file is stored

--- text_based_quiz_reader_of_outputted_file_of_quiz_creator_python_program.py
+++ text_based_quiz_reader_of_outputted_file_of_quiz_creator_python_program.py
@@ -1,10 +1,19 @@
 import os
 import json
+import tkinter as tk
+from tkinter import filedialog
 
-filename = "create_your_own_quiz.json"
-file_path = os.path.join(os.getcwd(), filename)
+root = tk.Tk()
+root.withdraw() 
+
+file_path = filedialog.askopenfilename(
+    title="Select a quiz file",
+    filetypes=[("JSON files", "*.json"), ("Text files", "*.txt")]
+)
 
 # Testing sample code for reading a text-based quiz file
 with open(file_path, "r", encoding="utf-8") as file:
     quiz_data = json.load(file)
+    
+    print(quiz_data)
 
